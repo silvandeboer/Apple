2f13c7c065f45a1f0270d5b8bc5d4d4ad53703a6
silvandeboer <s.deboer@student.utwente.nl>
2016-05-24 00:22:06 +0200

    Update MainActivity.java
    This code should be ran in the first Activity to initiate the database table with the recipes. DatabaseHandler and RecipeData should exist for this to run. There is a little demo part in here, take it out if you don't want it.

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package atlas.databasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,18 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create the recipes in the database table
         DatabaseHandler recipeTable = new DatabaseHandler(this);
-
         createData(recipeTable);
 
-
-        String lst = "";
+        //put all recipes in a list
+        Log.d("Reading: ", "Reading all contacts..");
         List<RecipeData> recipes = recipeTable.getAllRecipes();
 
-        for (RecipeData r : recipes){
-            lst+=r.getName()+"\n";
-        }
+        //Write all recipes in the log
+        for (RecipeData cn : recipes) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Type: " + cn.getType();
+            Log.d("Recipe: ", log);
+        };
 
+        //Write all recipes in the activity (FOR DEMO PURPOSES ONLY)
+        String lst = "";
+        for (RecipeData r : recipes){
+            lst+=r.getName()+" ("+r.getType()+")\n";
+        }
         TextView recipe_list = (TextView) findViewById(R.id.recipeList);
         recipe_list.setText(lst);
 
@@ -35,28 +43,41 @@ public class MainActivity extends AppCompatActivity {
     void createData(DatabaseHandler dbHandler){
 
         //clear table
-        //for (RecipeData r : dbHandler.getAllRecipes()){
-        //    dbHandler.deleteRecipe(r);
-        //}
+        for (RecipeData r : dbHandler.getAllRecipes()){
+            dbHandler.deleteRecipe(r);
+        }
 
-        //Write the recipes here
+        //Write the recipes here.
         //TODO: make a table as resource file
-        //List<RecipeData> rc_burger = new ArrayList<RecipeData>();
-        //rc_burger.add(new RecipeData("Burger+", "Lekker hoor"));
-        //rc_burger.add(new RecipeData("Burger", "Lekker hoor, maar minder vlees"));
-        //rc_burger.add(new RecipeData("Burger-", "Lekker hoor, maar zonder vlees"));
-        //rc_burger.add(new RecipeData("Burger--", "Gatverdamme kut! wat de fak is deze smerige poepzooi?!?"));
-
-        //add the recipes to the table
-        //dbHandler.addRecipeComplete(rc_burger);
-
-        RecipeData rcburger1 = new RecipeData("Burger",
+        List<RecipeData> rc_burger = new ArrayList<RecipeData>();
+        rc_burger.add(new RecipeData("Burger",
+                "Meat+",
+                "10g vegetables5mL water1 grain of salt",
+                "1. Take the massive piece of meat.2. Combine them.3. Add the rest.4. Eat!",
+                "American|Lunch",
+                "imageref",
+                "2","3","2","1","2","3","2"));
+        rc_burger.add(new RecipeData("Burger",
+                "Meat",
+                "10g vegetables5mL water1 grain of salt",
+                "1. Take the meat.2. Combine them.3. Add the rest.4. Eat!",
+                "American|Lunch",
+                "imageref",
+                "2","3","2","1","2","3","2"));
+        rc_burger.add(new RecipeData("Burger",
                 "Vegetarian",
                 "10g vegetables5mL water1 grain of salt",
                 "1. Take the vegetables.2. Combine them.3. Add the rest.4. Eat!",
                 "American|Lunch",
                 "imageref",
-                2,3,2,1,2,3,2);
-        dbHandler.addRecipe(rcburger1);
+                "2","3","2","1","2","3","2"));
+        rc_burger.add(new RecipeData("Burger",
+                "Vegan",
+                "1000g vegetables5mL water1 grain of salt",
+                "1. Take the vegetables.2. Combine them.3. Add the rest.4. Eat!",
+                "American|Lunch",
+                "imageref",
+                "2","3","2","1","2","3","2"));
+        dbHandler.addRecipeComplete(rc_burger);
     }
 }
