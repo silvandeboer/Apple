31a15226f93eb8b4d9f0cb84b3ea2b573f3f39e2
silvandeboer <s.deboer@student.utwente.nl>
2016-05-30 16:31:54 +0200

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,5 +1,6 @@
 package atlas.apple;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.database.sqlite.*;
 
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -62,6 +65,10 @@ public class Recipe extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+
+
+
+
     }
 
 
@@ -103,12 +110,31 @@ public class Recipe extends AppCompatActivity {
         private static final String ARG_RECIPE_NAME = "recipename";
         private static final String ARG_EXTENT = "extent";
         private static final String ARG_INGREDIENTS = "ingredients";
+        private static final String ARG_INSTRUCTIONS = "instructions";
+        private static final String ARG_TAGS = "tags";
+        private static final String ARG_IMAGE_REF = "imageref";
+        private static final String ARG_ENVIRONMENTAL_SCORE = "enviscore";
+        private static final String ARG_WATERFP_SCORE = "watescore";
+        private static final String ARG_CARBONFP_SCORE = "carbscore";
+        private static final String ARG_TIME_SCORE = "timescore";
+        private static final String ARG_DIFFICULTY_SCORE = "diffscore";
+        private static final String ARG_PRICE_SCORE = "pricscore";
+        private static final String ARG_RATING_SCORE = "ratiscore";
+
+
+        ImageButton environmentBtn;
+        ImageButton waterBtn;
+        ImageButton carbonBtn;
+        ImageButton timeBtn;
+        ImageButton difficultyBtn;
+        ImageButton priceBtn;
+        ImageButton rankingBtn;
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber, String recipeName, String extent, String ingredients) {
+        public static PlaceholderFragment newInstance(int sectionNumber, String recipeName, String extent, String ingredients, String instructions, String tags, String imageRef, String enviScore, String wateScore, String carbScore, String timeScore, String diffScore, String pricScore, String ratiScore) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             //THE ARGUMENTS THAT YOU GIVE WITH CREATING THE FRAGMENT,
@@ -116,6 +142,17 @@ public class Recipe extends AppCompatActivity {
             args.putString(ARG_RECIPE_NAME, recipeName);
             args.putString(ARG_EXTENT, extent);
             args.putString(ARG_INGREDIENTS, ingredients);
+            args.putString(ARG_INSTRUCTIONS, instructions);
+            args.putString(ARG_TAGS, tags);
+            args.putString(ARG_IMAGE_REF, imageRef);
+            args.putString(ARG_ENVIRONMENTAL_SCORE, enviScore);
+            args.putString(ARG_WATERFP_SCORE, wateScore);
+            args.putString(ARG_CARBONFP_SCORE, carbScore);
+            args.putString(ARG_TIME_SCORE, timeScore);
+            args.putString(ARG_DIFFICULTY_SCORE, diffScore);
+            args.putString(ARG_PRICE_SCORE, pricScore);
+            args.putString(ARG_RATING_SCORE, ratiScore);
+
             fragment.setArguments(args);
             return fragment;
         }
@@ -128,14 +165,79 @@ public class Recipe extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_recipe, container, false);
 
             //Change the widgets in the tab
-            TextView vw_name = (TextView) rootView.findViewById(R.id.recipe_name);
-            vw_name.setText(getArguments().getString(ARG_RECIPE_NAME));
+            TextView vw_name = (TextView) rootView.findViewById(R.id.titleRecipe);
+            vw_name.setText(getArguments().getString(ARG_EXTENT)+" "+getArguments().getString(ARG_RECIPE_NAME));
 
-            TextView vw_extent = (TextView) rootView.findViewById(R.id.extent);
-            vw_extent.setText(getArguments().getString(ARG_EXTENT));
+            //Still missing
+            //TextView vw_extent = (TextView) rootView.findViewById(R.id.extent);
+            //vw_extent.setText(getArguments().getString(ARG_EXTENT));
 
             TextView vw_ingredients = (TextView) rootView.findViewById(R.id.ingredients);
             vw_ingredients.setText(getArguments().getString(ARG_INGREDIENTS));
+
+            TextView vw_instructions = (TextView) rootView.findViewById(R.id.instructions);
+            vw_instructions.setText(getArguments().getString(ARG_INSTRUCTIONS));
+
+
+            //POP-UP'S FOR INFORMATION IN THE INFORMATION SECTION
+            //TODO: add buttons/images dependent on score
+
+            environmentBtn = (ImageButton) rootView.findViewById(R.id.environmentBtn);
+            environmentBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupEnvironment.class));
+                }
+            });
+
+            waterBtn = (ImageButton) rootView.findViewById(R.id.waterBtn);
+            waterBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupWater.class));
+                }
+            });
+
+            carbonBtn = (ImageButton) rootView.findViewById(R.id.carbonBtn);
+            carbonBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupCarbon.class));
+                }
+            });
+
+            timeBtn = (ImageButton) rootView.findViewById(R.id.timeBtn);
+            timeBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupTime.class));
+                }
+            });
+
+            difficultyBtn = (ImageButton) rootView.findViewById(R.id.difficultyBtn);
+            difficultyBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupDifficulty.class));
+                }
+            });
+
+            priceBtn = (ImageButton) rootView.findViewById(R.id.priceBtn);
+            priceBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupPrice.class));
+                }
+            });
+
+            rankingBtn = (ImageButton) rootView.findViewById(R.id.ratingBtn);
+            rankingBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)
+                {
+                    startActivity(new Intent(getContext(), PopupRanking.class));
+                }
+            });
+
 
             return rootView;
         }
@@ -158,7 +260,7 @@ public class Recipe extends AppCompatActivity {
 
             //The content of the recipe tab is loaded here
             RecipeData recipe = recipeTable.getRecipe(position+1);
-            return PlaceholderFragment.newInstance(position, recipe.getName(), recipe.getType(), recipe.getIngredients());
+            return PlaceholderFragment.newInstance(position, recipe.getName(), recipe.getType(), recipe.getIngredients(), recipe.getInstructions(), recipe.getTags(), recipe.getImageRef(), recipe.getEnvironmentalScore(), recipe.getWaterfpScore(), recipe.getCarbonfpScore(), recipe.getTimeScore(), recipe.getDifficultyScore(), recipe.getPriceScore(), recipe.getRatingScore());
         }
 
         @Override
